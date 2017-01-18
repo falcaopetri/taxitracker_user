a571445e94f87df4a2f44bb007d68519682aeebe
José Vitor <jvcaquino95@gmail.com>
2017-01-18 21:41:03 -0200

    Social Login

--- app/src/main/java/br/ufscar/dc/mds/taxitracker_user/MainActivity.java
+++ app/src/main/java/br/ufscar/dc/mds/taxitracker_user/MainActivity.java
@@ -1,18 +1,30 @@
 package br.ufscar.dc.mds.taxitracker_user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.UnsupportedEncodingException;
 
 import br.ufscar.dc.mds.taxitracker_library.TaxiTrackerRestClientUsage;
 import br.ufscar.dc.mds.taxitracker_library.TaxiTrackerRestHandler;
 
-public class MainActivity extends AppCompatActivity implements TaxiTrackerRestHandler {
+public class MainActivity extends AppCompatActivity implements TaxiTrackerRestHandler, View.OnClickListener{
     TaxiTrackerRestClientUsage taxiTrackerRest;
     private final static String AUTH_USER_TAG = "passageiro";
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth auth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,33 @@ public class MainActivity extends AppCompatActivity implements TaxiTrackerRestHa
         setContentView(R.layout.activity_main);
 
         taxiTrackerRest = new TaxiTrackerRestClientUsage(this);
+        auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null){
+            //user already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+        } else {
+            startActivityForResult(AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setProviders(
+                            AuthUI.GOOGLE_PROVIDER)
+                    .build(), RC_SIGN_IN);
+        }
+
+        findViewById(R.id.log_out_button).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                //user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+            } else {
+                //user not authnticated
+                Log.d("AUTH", "NOT AUTHENTICATED");
+            }
+        }
     }
 
     public void login(View view) {
@@ -48,6 +87,21 @@ public class MainActivity extends AppCompatActivity implements TaxiTrackerRestHa
             taxiTrackerRest.createRace(this, "origem", "destino");
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.log_out_button){
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("AUTH", "USER LOGGED OUT");
+                            finish();
+                        }
+                    }  );
         }
     }
 }
